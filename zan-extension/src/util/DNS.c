974063f46564b4cbcf399d0bd400949804a82f16
huyanlin <huyanlin@youzan.com>
2017-07-11 21:13:27 +0800

    fix some compiling warning on centos

--- zan-extension/src/util/DNS.c
+++ zan-extension/src/util/DNS.c
@@ -142,7 +142,7 @@ static int swDNSResolver_onReceive(swReactor *reactor, swEvent *event)
 {
     swDNSResolver_header *header = NULL;
     swClient *cli = NULL;
-    swDNSQ_FLAGS *qflags = NULL;
+    //swDNSQ_FLAGS *qflags = NULL;
     swDNSRR_FLAGS *rrflags = NULL;
 
     char packet[SW_BUFFER_SIZE_UDP] = {0};
@@ -173,7 +173,7 @@ static int swDNSResolver_onReceive(swReactor *reactor, swEvent *event)
     swDNSResolver_domain_decode(_domain_name);
     steps = steps + (strlen(_domain_name) + 2);
 
-    qflags = (swDNSQ_FLAGS *) &packet[steps];
+    //qflags = (swDNSQ_FLAGS *) &packet[steps];
     steps = steps + sizeof(swDNSQ_FLAGS);
 
     //printf("ancount=%d, nscount=%d, qdcount=%d, arcount=%d\n", ntohs(header->ancount), ntohs(header->nscount), ntohs(header->qdcount), ntohs(header->arcount));
@@ -555,7 +555,7 @@ static int get_host_by_syscall(int af,char* name,swDNS_cache* cache)
 	hints.ai_protocol = IPPROTO_TCP;
 
 	if (getaddrinfo(name,NULL,&hints,&result) != 0 || !result){
-		swWarn("get address result(%d) info error.%s",result? (int)result:0,gai_strerror(errno));
+		swWarn("get address result(%ld) info error.%s",result? (long)result:0,gai_strerror(errno));
 		if (result) freeaddrinfo(result);
 		return SW_ERR;
 	}
