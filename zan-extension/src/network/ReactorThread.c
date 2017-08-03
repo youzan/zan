14a10304c0023eb8c9292baa024fd506e27b8959
maben <www.maben@foxmail.com>
2017-08-03 15:22:41 +0800

    fix bug

--- zan-extension/src/network/ReactorThread.c
+++ zan-extension/src/network/ReactorThread.c
@@ -968,6 +968,7 @@ int swReactorThread_onClose(swReactor *reactor, swEvent *event)
     }
     else if (reactor->del(reactor, fd) == 0)
     {
+        swReactorThread_close(reactor, fd);
         return SwooleG.factory->notify(SwooleG.factory, &notify_ev);
     }
     else
